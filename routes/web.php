d91f35cefdd8b3b6a9133f939c8de48fa0d61bb1
Ajirin <mubarakolagoke@gmail.com>
2020-09-14 01:11:02 +0100

    tinymce handler moved from api to web

--- routes/web.php
+++ routes/web.php
@@ -20,6 +20,43 @@ Route::post('/register-next', function(Request $request){
 
 Auth::routes();
 
+Route::post('/upload-tinymce', function(Request $request){
+    $accepted_origin = array("http://localhost:8000", "http://transcript.mudospharmacy.com");
+    
+    if($request->has('file')){
+        $image = $request->file('file');
+        $image_name = $image->getClientOriginalName();
+
+        if(isset($_SERVER['HTTP_ORIGIN'])){
+            if(in_array($_SERVER['HTTP_ORIGIN'], $accepted_origin)){
+                header('Access-Control-Allow-Origin' . $_SERVER['HTTP_ORIGIN']);
+            }else{
+                header('HTTP/1.1 403 Origin Denied');
+                return;
+            }
+        }
+
+        if(preg_match("/([^\w\s\d\-_~,;:\[\]\(\).])|([\.]{2,})/", $image_name)){
+            header("HTTP/1.1 400 Invalid file name.");
+            return;
+        }
+
+        if(!in_array(strtolower(pathinfo($image_name, PATHINFO_EXTENSION)), array('gif', 'jpg', 'png', 'jpeg'))){
+            header("HTTP/1/1 400 Invalid extention");
+            return;
+        }
+
+        $filetowrite = $request->file('file')->storeAs('public/uploads', $image_name );
+
+        $location = "/storage/uploads/".$image_name;
+        echo json_encode(array('location' => $location));
+    }else{
+        header("HTTP/1.1 500 server Error");
+    }
+
+});
+
+
 Route::prefix('admin')->group(function(){
         Route::get('dashboard', 'Admin\dashboardController@dashboard');
         Route::get('calender', 'Admin\calendarController@showCalender');
